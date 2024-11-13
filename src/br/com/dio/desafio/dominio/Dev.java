617488464c51f5ab2dde762454449c4f1fd8b731
FERNANDO SANTOS <fernandopernambuco@gmail.com>
2024-11-13 15:08:12 -0300

    Update Dev.java

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,32 +1,81 @@
 package br.com.dio.desafio.dominio;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
-        // Criando um Bootcamp
-        Bootcamp bootcampJava = new Bootcamp();
-        bootcampJava.setNome("Bootcamp Java");
-        bootcampJava.setDescricao("Aprenda Java do básico ao avançado!");
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-        // Criando conteúdos e adicionando ao Bootcamp
-        Curso cursoJava = new Curso("Curso de Java", "Aprenda os conceitos básicos de Java", 8);
-        bootcampJava.getConteudos().add(cursoJava);
+    // Inscrever o Dev em um Bootcamp
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        if (bootcamp != null) {
+            this.conteudosInscritos.addAll(bootcamp.getConteudos());
+            bootcamp.getDevsInscritos().add(this);
+        } else {
+            System.err.println("O Bootcamp não pode ser nulo.");
+        }
+    }
 
-        // Criando um Dev
-        Dev dev = new Dev();
-        dev.setNome("João");
+    // Progredir no Bootcamp (avançar para o próximo conteúdo)
+    public void progredir() {
+        // Pega o primeiro conteúdo inscrito (se houver)
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        conteudo.ifPresent(c -> {
+            this.conteudosConcluidos.add(c);
+            this.conteudosInscritos.remove(c);
+        });
 
-        // Inscrevendo o Dev no Bootcamp
-        dev.inscreverBootcamp(bootcampJava);
+        if (conteudo.isEmpty()) {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-        // Exibindo os conteúdos inscritos
-        System.out.println("Conteúdos inscritos no Bootcamp: " + dev.getConteudosInscritos());
+    // Calcular o total de XP baseado nos conteúdos concluídos
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream()
+                .mapToDouble(Conteudo::calcularXp) // Mapeia cada conteúdo para seu XP
+                .sum(); // Soma todos os XP dos conteúdos concluídos
+    }
 
-        // Dev progredindo
-        dev.progredir();
-        System.out.println("Conteúdos concluídos pelo Dev: " + dev.getConteudosConcluidos());
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
 
-        // Calculando o total de XP
-        System.out.println("Total de XP: " + dev.calcularTotalXp());
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    // Métodos equals e hashCode para comparar Devs de forma correta
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && 
+               Objects.equals(conteudosInscritos, dev.conteudosInscritos) && 
+               Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
