bc5865c2848e336c16d0b2f5f15ca76fd4dad628
FERNANDO SANTOS <fernandopernambuco@gmail.com>
2024-11-13 15:04:30 -0300

    Update Mentoria.java

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,18 +2,23 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo{
+public class Mentoria extends Conteudo {
 
     private LocalDate data;
 
+    // Construtor com parâmetros para inicializar a mentoria com título, descrição e data
+    public Mentoria(String titulo, String descricao, LocalDate data) {
+        setTitulo(titulo);
+        setDescricao(descricao);
+        this.data = data;
+    }
+
     @Override
     public double calcularXp() {
-        return XP_PADRAO + 20d;
+        return XP_PADRAO + 20d; // XP padrão + 20 para mentoria
     }
 
-    public Mentoria() {
-    }
-
+    // Getters e setters
     public LocalDate getData() {
         return data;
     }
@@ -22,6 +27,7 @@ public class Mentoria extends Conteudo{
         this.data = data;
     }
 
+    // Método toString() para exibir os detalhes da mentoria
     @Override
     public String toString() {
         return "Mentoria{" +
