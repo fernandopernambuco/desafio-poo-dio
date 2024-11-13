24d1f7538a03c5be905fb0e7f947ab9ea3172c9c
FERNANDO SANTOS <fernandopernambuco@gmail.com>
2024-11-13 14:56:29 -0300

    Update Conteudo.java

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -2,13 +2,22 @@ package br.com.dio.desafio.dominio;
 
 public abstract class Conteudo {
 
+    // Constante para XP padrão
     protected static final double XP_PADRAO = 10d;
 
     private String titulo;
     private String descricao;
 
+    // Construtor
+    public Conteudo(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    // Método abstrato para calcular XP (deve ser implementado pelas subclasses)
     public abstract double calcularXp();
 
+    // Getters e Setters
     public String getTitulo() {
         return titulo;
     }
@@ -20,6 +29,21 @@ public abstract class Conteudo {
     public String getDescricao() {
         return descricao;
     }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // Método toString para representar o conteúdo de maneira legível
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
+
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
